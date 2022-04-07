569508db4ca376dee98f932a5802acfb4f8ecf1b
greendrop <greendrop.devel@gmail.com>
2022-04-08 07:44:51 +0900

    add annotate

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,29 @@
+# ## Schema Information
+#
+# Table name: `tasks`
+#
+# ### Columns
+#
+# Name               | Type               | Attributes
+# ------------------ | ------------------ | ---------------------------
+# **`id`**           | `integer`          | `not null, primary key`
+# **`user_id`**      | `integer`          | `not null`
+# **`title`**        | `string`           |
+# **`description`**  | `text`             |
+# **`done`**         | `boolean`          | `default(FALSE), not null`
+# **`created_at`**   | `datetime`         | `not null`
+# **`updated_at`**   | `datetime`         | `not null`
+#
+# ### Indexes
+#
+# * `index_tasks_on_user_id`:
+#     * **`user_id`**
+#
+# ### Foreign Keys
+#
+# * `user_id`:
+#     * **`user_id => users.id`**
+#
 class Task < ApplicationRecord
   belongs_to :user
 
